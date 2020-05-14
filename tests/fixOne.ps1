c86e4e2866890b6bf0926c2be987d77be86a44bc
Alex Buck <abuck@microsoft.com>
2020-05-14 12:49:54 -0400

    Abuck/review 1239 (#535)
    * Update .gitignore
    
    * Fixes

--- tests/fixOne.ps1
+++ tests/fixOne.ps1
@@ -1,5 +1,5 @@
 . .\Test-Includes.ps1
 . .\Test-Paths.ps1
 
-$file = $(Get-Item "../docs/organize/central-it.md") 
+$file = $(Get-Item "../docs/ready/landing-zone/index.md") 
 Test-AllLocalPaths @($file) $true
